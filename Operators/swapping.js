ca23e0686c1a33ba4b8b06e77ae2109949fae4a6
Antonio Rosario <antonio.rosario340@gmail.com>
2025-06-14 09:59:55 -0400

    SwappingValues

---
+++ Operators/swapping.js
@@ -0,0 +1,19 @@
+// creating a variable and storing a value of red
+
+//varible is assigned red
+let a= 'red'
+
+//varible is assigned blue
+let b= 'blue'
+
+// variable c should be assigned a which is red.
+let c=a;
+
+//variable a is assigned blue
+a=b;
+// variable b now equals red
+b=c;
+
+
+console.log (a)
+console.log (b)
