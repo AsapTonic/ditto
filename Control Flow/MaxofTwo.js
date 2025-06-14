ea0fec5c7fa99f4bbf39f39f71959910a5eb090e
Antonio Rosario <antonio.rosario340@gmail.com>
2025-06-14 10:20:22 -0400

    MaxofTwo

---
+++ Control Flow/MaxofTwo.js
@@ -0,0 +1,16 @@
+
+let number= max (1,2);
+console.log (number);
+
+// function to return the max value between the two parameters
+function max (a,b){
+
+
+    //if a is greater than b return a, if not return b
+    if (a>b)
+        return a;
+    else return b;
+
+}
+
+//function should return 2
